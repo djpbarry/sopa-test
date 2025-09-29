2cab4c7a842f7f2e8d2c65f2095689fa5dd3f38f
David Barry <david.barry@crick.ac.uk>
2025-09-29 14:21:44 +0100

    Allow overwrite of preexisting data

--- run_sopa.py
+++ run_sopa.py
@@ -5,7 +5,7 @@ imagepath = "/nemo/stp/lm/inputs/lil/ForDave/EHP601_25_1_Stitched.ome.tif"
 
 dataset = sopa.io.ome_tif(imagepath, as_image=False)
 
-dataset.write("./demo.zarr")
+dataset.write("./demo.zarr", overwrite=True)
 
 dataset = spatialdata.read_zarr("demo.zarr")  # we can read the data back
 
@@ -17,6 +17,6 @@ channels = sopa.utils.get_channel_names(dataset)
 
 print(channels)
 
-sopa.segmentation.cellpose(dataset, "DAPI (DAPI)", diameter=35, key_added="nuclei", gpu=True)
+sopa.segmentation.cellpose(dataset, "DAPI (DAPI)", diameter=35, key_added="nuclei", gpu=True, overwrite=True)
 
 print("Done")
