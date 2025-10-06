e4419dc0878865b2aaee28572c30bb8df6242948
David Barry <david.barry@crick.ac.uk>
2025-10-06 15:23:04 +0100

    Added functionality to cluster results - haven't quite figured out this part yet!

--- run_sopa.py
+++ run_sopa.py
@@ -1,44 +1,71 @@
+import scanpy as sc
 import sopa
 import spatialdata
+import squidpy as sq
 
-imagepath = "./EHP601_25_1_Stitched_cropped.tif"
+#imagepath = "./EHP601_25_1_Stitched_cropped.tif"
 
-print("Opening image")
+#print("Opening image")
 
-dataset = sopa.io.ome_tif(imagepath, as_image=False)
+#dataset = sopa.io.ome_tif(imagepath, as_image=False)
 
-print("Saving as Zarr...")
+#print("Saving as Zarr...")
 
-dataset.write("./demo.zarr", overwrite=True)
+#dataset.write("./demo.zarr", overwrite=True)
 
 print("Loading Zarr...")
 
 dataset = spatialdata.read_zarr("./demo.zarr")  # we can read the data back
 
-print("Make image patches...")
+#print("Make image patches...")
 
-sopa.make_image_patches(dataset)
+#sopa.make_image_patches(dataset)
 
 print("Set dask backend...")
 
 sopa.settings.parallelization_backend = "dask"
 
-print("Get channel names...")
+#print("Get channel names...")
 
-channels = sopa.utils.get_channel_names(dataset)
+#channels = sopa.utils.get_channel_names(dataset)
 
-print(channels)
+#print(channels)
 
-print("Run stardist...")
+#print("Run stardist...")
 
-sopa.segmentation.stardist(dataset, model_type='2D_versatile_fluo', channels=str(channels[0]))
+#sopa.segmentation.stardist(dataset, model_type='2D_versatile_fluo', channels=str(channels[0]))
 
-print("Aggregating...")
+#print("Aggregating...")
 
-sopa.aggregate(dataset)
+#sopa.aggregate(dataset)
+adata = dataset['table']
 
-print("Generating report...")
+#print("Generating report...")
 
-sopa.io.write_report("report.html", dataset)
+#sopa.io.write_report("report.html", dataset)
+
+# Step 1: Preprocess and cluster based on intensity data
+# Normalize the data
+sc.pp.normalize_total(adata)
+
+# Compute PCA for dimensionality reduction
+sc.tl.pca(adata)
+
+# Compute neighborhood graph
+sc.pp.neighbors(adata, n_neighbors=15, n_pcs=10)
+
+sc.tl.umap(adata)
+
+# Using the igraph implementation and a fixed number of iterations can be significantly faster,
+# especially for larger datasets
+sc.tl.leiden(adata, flavor="igraph", n_iterations=2)
+
+sc.pl.umap(adata, color=["leiden"])
+
+print("Saving as Zarr...")
+
+dataset.write("./demo.zarr", overwrite=True)
+
+sq.pl.spatial_scatter(adata)
 
 print("Done")
